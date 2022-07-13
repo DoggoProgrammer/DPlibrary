e91e7acbd8c06c407450f03b8bfb29978869a509
DoggoProgrammer <85500383+DoggoProgrammer@users.noreply.github.com>
2022-07-13 16:31:17 +0200

    Create DPClientconsole.lua

---
+++ scripts/D/DPClient/DPClientconsole.lua
@@ -0,0 +1,10 @@
+local Client = {}
+local Clprint = rconsoleprint
+local Clinput = rconsoleinput
+local Clname = rconsolename
+local Clclear = rconsoleclear
+Clname("DPClient - V0.1 TEST VERSION - Console Edition")
+Clprint("Hello this is DPClient On Version 0.1. Please mind that RT (Redirect To) is not available currently.\nCurrent Plans:\nV0.2 - Testing RT\nV1.0 - RT is available")
+task.wait(5)
+Clclear()
+Clprint("Checking Files...")
