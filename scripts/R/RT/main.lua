e4dce51fec92f0f7e436780b9639edf507499b9e
DoggoProgrammer <85500383+DoggoProgrammer@users.noreply.github.com>
2022-07-15 18:43:49 +0200

    Update main.lua

--- scripts/R/RT/main.lua
+++ scripts/R/RT/main.lua
@@ -1,3 +1,12 @@
 -->> rt api
-local Clprint = rconsoleprint; local Clinput = rconsoleinput; local Clname = rconsolename; local Clclear = rconsoleclear;
-Clname('RT')
+-->> General
+local function req(name)
+    if (name == 'APP') then
+        return game:HttpGet('')
+    end
+end
+-->> Settings
+local APPLICATION = req('APP') -->> code gui
+local CODE = {raw = '', class = '', http_requests = false} -->> class is the script listed in a list (script1, script2)
+local function update(new) CODE = {new.raw, new.class, new.http_requests} end -->> update is being used when the player launches a script so the var CODE can be updated
+-->> Other Functions
