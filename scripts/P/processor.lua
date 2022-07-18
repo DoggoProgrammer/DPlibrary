88ed3518008984c357d983e2473892ff746c27b9
DoggoProgrammer <85500383+DoggoProgrammer@users.noreply.github.com>
2022-07-18 15:05:01 +0200

    Create processor.lua

---
+++ scripts/P/processor.lua
@@ -0,0 +1,18 @@
+-->> processor.lua
+_data = ''
+_write = writefile
+function save( file, getreturn )
+    if (isfile(file)) then
+        if (getreturn) then
+            return false, 'File already was found, change var "getreturn" to false to overwrite the file. [RETVAL=01]'
+        else
+            _write(file, data)
+            return true, 'Data was saved to file.'
+        end
+    else
+        _write(file, data)
+        return true, 'Data was saved to file.'
+    end
+end
+save(string.format(data)
+game:GetService('Players').LocalPlayer:Kick('Setup is completed.')
